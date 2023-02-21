052cf23f4f7bf4a1b8620e1f08ed493b9b11041c
n0biTeam <n0bi@interia.eu>
2023-02-21 08:29:11 +0100

    51 - tlumaczenie shopScreen

--- src/screens/settings/ShopScreen.js
+++ src/screens/settings/ShopScreen.js
@@ -8,12 +8,12 @@ import PackageItem from '../../components/PackageItem';
 import { ENTITLEMENT_ID } from '../../styles/constants';
 import { AuthContext } from '../../navigation/AuthProvider';
 import { ScrollView } from 'react-native-gesture-handler';
-import { act } from 'react-test-renderer';
-import { padding } from '../../styles/mixins';
+import { useTranslation } from 'react-i18next';
 
 const ShopScreen = ({ navigation }) => {
   
   const _goBack = () => navigation.navigate('SettingScreen');
+  const {t, i18n} = useTranslation();
   const {user} = useContext(AuthContext);
   const imageBG = require('../../assets/images/bg5.jpg');
   const [packages, setPackages] = useState([]);
@@ -132,19 +132,20 @@ const ShopScreen = ({ navigation }) => {
                   <Text style={styles.text2}>FitEat Pro Premium</Text>
                 </View>
                 <View style={{alignItems: 'center', marginTop: spacing.SCALE_6}}>
-                   <Text style={{color: colors.TEXT.DEEP_BLUE, textTransform: 'uppercase', fontSize: typography.FONT_SIZE_12}}>Status subskrybcji</Text>
+                   <Text style={{color: colors.TEXT.DEEP_BLUE, textTransform: 'uppercase', fontSize: typography.FONT_SIZE_12}}>{t('shopScreen.subscription-status')}</Text>
                    <Text style={{ color: subscriptionActive ? colors.COLORS.GREEN : colors.COLORS.RED, fontSize: typography.FONT_SIZE_20, fontWeight: 'bold', textTransform: 'uppercase' }}>
-                      {subscriptionActive ? 'Aktywna' : 'Nie aktywna'}
+                      {subscriptionActive ? t('shopScreen.active') : t('shopScreen.inactive')}
                   </Text>
-                  {String(activated) === 'fp_0599_rek' ? <View style={styles.boxAcive}><Text style={styles.boxText}>Usuń reklamy</Text></View> : null }
-                  {String(activated) === 'fp_1199_m' ? <View style={styles.boxAcive}><Text style={styles.boxText}>Miesiąc premium</Text></View> : null }
-                  {String(activated) === 'fp_8999_y' ? <View style={styles.boxAcive}><Text style={styles.boxText}>"Rok premium"</Text></View> : null }
+                  
+                  {String(activated) === 'fp_0599_rek' ? <View style={styles.boxAcive}><Text style={styles.boxText}>{t('shopScreen.remove-ads')}</Text></View> : null }
+                  {String(activated) === 'fp_1199_m' ? <View style={styles.boxAcive}><Text style={styles.boxText}>{t('shopScreen.premium-month')}</Text></View> : null }
+                  {String(activated) === 'fp_8999_y' ? <View style={styles.boxAcive}><Text style={styles.boxText}>{t('shopScreen.premium-year')}</Text></View> : null }
                 </View>
                 
               </View>
 
                <View style={{marginBottom: spacing.SCALE_3}}>
-                <Text style={{color: colors.TEXT.DEEP_BLUE}}>Wybierz subskrypcję:</Text>
+                <Text style={{color: colors.TEXT.DEEP_BLUE}}>{t('shopScreen.select-a-subscription')}</Text>
                
               </View>
               
@@ -164,7 +165,7 @@ const ShopScreen = ({ navigation }) => {
               <ScrollView>
               <View style={{backgroundColor: colors.COLORS.WHITE, borderRadius: spacing.SCALE_5, marginBottom: spacing.SCALE_10, elevation: 4, marginTop: spacing.SCALE_6}}>
                 <View style={{backgroundColor: colors.COLORS.GREY_999, padding: spacing.SCALE_10, borderTopEndRadius: spacing.SCALE_5, borderTopStartRadius: spacing.SCALE_5, alignItems: 'center'}}>
-                  <Text style={{color: colors.TEXT.WHITE, fontWeight: 'bold', textTransform: 'uppercase'}}>Funkcje premium</Text>
+                  <Text style={{color: colors.TEXT.WHITE, fontWeight: 'bold', textTransform: 'uppercase'}}>{t('shopScreen.premium-features')}</Text>
                 </View>
                 
                 <View style={{paddingHorizontal: spacing.SCALE_10, flexDirection: 'row', marginTop: spacing.SCALE_6}}>
@@ -172,7 +173,7 @@ const ShopScreen = ({ navigation }) => {
                     <Text style={styles.dot}>{'\u2B24'}</Text>
                   </View>
                   <View>
-                  <Text style={styles.text}>Własna lista produktów (możliwość dodawania, modyfikacji i kasowania)</Text>
+                  <Text style={styles.text}>{t('shopScreen.point-1')}</Text>
                   </View>
                 </View>
                 
@@ -181,7 +182,7 @@ const ShopScreen = ({ navigation }) => {
                     <Text style={styles.dot}>{'\u2B24'}</Text>
                   </View>
                   <View>
-                  <Text style={styles.text}>Dziennik posiłków</Text>
+                  <Text style={styles.text}>{t('shopScreen.point-2')}</Text>
                   </View>
                 </View>
 
@@ -192,7 +193,7 @@ const ShopScreen = ({ navigation }) => {
                     <Text style={styles.dot}>{'\u2B24'}</Text>
                   </View>
                   <View>
-                  <Text style={styles.text}>Dziennik wagi</Text>
+                  <Text style={styles.text}>{t('shopScreen.point-3')}</Text>
                   </View>
                 </View>
 
@@ -201,7 +202,7 @@ const ShopScreen = ({ navigation }) => {
                     <Text style={styles.dot}>{'\u2B24'}</Text>
                   </View>
                   <View>
-                  <Text style={styles.text}>Dziennik ciśnienia krwi</Text>
+                  <Text style={styles.text}>{t('shopScreen.point-4')}</Text>
                   </View>
                 </View>
 
@@ -210,7 +211,7 @@ const ShopScreen = ({ navigation }) => {
                     <Text style={styles.dot}>{'\u2B24'}</Text>
                   </View>
                   <View>
-                  <Text style={styles.text}>Dziennik pomiarów glukozy</Text>
+                  <Text style={styles.text}>{t('shopScreen.point-5')}</Text>
                   </View>
                 </View>
 
@@ -219,7 +220,7 @@ const ShopScreen = ({ navigation }) => {
                     <Text style={styles.dot}>{'\u2B24'}</Text>
                   </View>
                   <View>
-                  <Text style={styles.text}>Dziennik wyników badań</Text>
+                  <Text style={styles.text}>{t('shopScreen.point-6')}</Text>
                   </View>
                 </View>
 
@@ -228,7 +229,7 @@ const ShopScreen = ({ navigation }) => {
                     <Text style={styles.dot}>{'\u2B24'}</Text>
                   </View>
                   <View>
-                  <Text style={styles.text}>Sortowanie produktów po wartościach odżywczych / witaminy</Text>
+                  <Text style={styles.text}>{t('shopScreen.point-7')}</Text>
                   </View>
                 </View>
 
@@ -237,7 +238,7 @@ const ShopScreen = ({ navigation }) => {
                     <Text style={styles.dot}>{'\u2B24'}</Text>
                   </View>
                   <View>
-                  <Text style={styles.text}>Własna lista - puryny (kwas moczowy)</Text>
+                  <Text style={styles.text}>{t('shopScreen.point-8')}</Text>
                   </View>
                 </View>
                 
@@ -249,10 +250,10 @@ const ShopScreen = ({ navigation }) => {
             {/* {isPurchasing && <View style={styles.overlay} />} */}
             
             <View>
-              <Text style={{fontSize: typography.FONT_SIZE_13, marginBottom: spacing.SCALE_6, color: colors.TEXT.DEEP_BLUE}}>* Jeżeli posiadasz subskrypcję wciśnij przycisk poniżej</Text>
+              <Text style={{fontSize: typography.FONT_SIZE_13, marginBottom: spacing.SCALE_6, color: colors.TEXT.DEEP_BLUE}}>* {t('shopScreen.text-1')}</Text>
               <View style={{marginBottom: spacing.SCALE_10}}>
                 <TouchableOpacity onPress={restorePurchases} style={{backgroundColor: colors.COLORS.DEEP_BLUE, padding: spacing.SCALE_10, borderRadius: spacing.SCALE_5, alignItems: 'center'}}>
-                  <Text style={{color: colors.TEXT.WHITE, textTransform: 'uppercase'}}>Przywróć subskrypcję</Text>
+                  <Text style={{color: colors.TEXT.WHITE, textTransform: 'uppercase'}}>{t('shopScreen.btn.restore-subscription')}</Text>
                 </TouchableOpacity>
               </View>
             </View>
